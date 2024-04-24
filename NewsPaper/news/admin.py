4fa236d7959770149449f8c12429984f9a5ac2d0
AlexKhudyakov83 <askhudyakov83@yandex.ru>
2024-04-24 20:10:34 +0300

    snouta 24.04 настройка админки

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Author, Category, Post, PostCategory, Comment
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'rating', 'categoryType')
+    list_filter = ('title', 'rating')
+    search_fields = ('rating',)
+
+
 admin.site.register(Author)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
